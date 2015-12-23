06aac1e79e140200bc29168dc4c43af54a8c9751
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-12-23 21:53:20 +0200

    fix columns and row values

--- src/js/gold.js
+++ src/js/gold.js
@@ -32,10 +32,10 @@ addEl(100,10,2);
 
 function parseMap(map){
         var freeCoordinats = [];
-        var columns = map.length;
-        console.log(columns);
-        var rows = map[1].length;
-        console.log(rows);
+        var rows = map.length;
+        console.log('columns = '+columns);
+        var columns = map[1].length;
+        console.log('rows = '+rows);
 
 
     for (var i = 0; i < columns; i++) {
@@ -69,6 +69,44 @@ function parseMap(map){
     //}
 }
 
+function parseMap(map){
+    var freeCoordinats = [];
+    var rows = map.length;
+    console.log('columns = '+columns);
+    var columns = map[1].length;
+    console.log('rows = '+rows);
+
+
+    for (var i = 0; i < rows; i++) {
+
+        console.log(map[i]);
+
+
+        for (var j = 0; j < columns; j++) {
+
+            console.log(map[i][j]);
+            if (map[i][j] == 0) {
+                freeCoordinats.push(map[i][j]);
+            }
+        }
+
+    }
+    console.log(freeCoordinats);
+    return freeCoordinats;
+    //for (var i = 0; i < 1; i++) {
+    //    console.log(map[1][1].length);
+    //}
+    //console.log('colums = '+colums);
+    //console.log('rows = '+rows);
+
+
+    //for (var i = 0; i < map.length; i++) {
+    //    console.log(map[i]);
+    //    for (var j = 0; j < map[i].length; j++) {
+    //
+    //    }
+    //}
+}
 
 var loop = parseMap(map);
 
