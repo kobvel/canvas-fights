4f376797df11075781f67345def4aeee7be339f3
kobvel <kobvel@gmail.com>
2015-12-23 21:06:30 +0200

    basic setup

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -8,7 +8,7 @@ var GulpConfig = (function(){
     };
 
     this.app = {
-      js:     ['./src/js/**/*.js'],
+      js:     ['./src/js/BaseModel/baseModel.js', './src/js/godservice/godservice.js', './src/js/app.js'],
       css:    ['./src/css/*.css'],
       index:  ['./src/index.html']
     };
